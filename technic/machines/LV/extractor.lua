e03c6e16fd7cc0650ab2554b7f892ac0b579a91e
SX <sx@minetest>
2020-12-11 16:18:59 +0200

    Alternative extractor recipe without tree tap

--- technic/machines/LV/extractor.lua
+++ technic/machines/LV/extractor.lua
@@ -1,13 +1,28 @@
 
 minetest.register_alias("extractor", "technic:lv_extractor")
 
-minetest.register_craft({
-	output = 'technic:lv_extractor',
-	recipe = {
-		{'technic:treetap', 'basic_materials:motor',          'technic:treetap'},
-		{'technic:treetap', 'technic:machine_casing', 'technic:treetap'},
-		{'',                'technic:lv_cable',       ''},
-	}
-})
+if technic.config:get_bool("enable_tree_tap") then
+
+	minetest.register_craft({
+		output = 'technic:lv_extractor',
+		recipe = {
+			{'technic:treetap', 'basic_materials:motor',          'technic:treetap'},
+			{'technic:treetap', 'technic:machine_casing', 'technic:treetap'},
+			{'',                'technic:lv_cable',       ''},
+		}
+	})
+
+else
+
+	minetest.register_craft({
+		output = 'technic:lv_extractor',
+		recipe = {
+			{'basic_materials:motor', 'technic:wrought_iron_ingot', 'basic_materials:motor'},
+			{'technic:wrought_iron_ingot', 'technic:machine_casing', 'technic:wrought_iron_ingot'},
+			{'', 'technic:lv_cable', ''},
+		}
+	})
+
+end
 
 technic.register_extractor({tier = "LV", demand = {300}, speed = 1})
