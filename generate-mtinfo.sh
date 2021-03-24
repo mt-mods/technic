75f30d4ddaae673087d555567f552c6a0c39738c
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2021-03-24 19:39:14 +0100

    add moreblocks to mtinfo mods (#167)
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>

--- generate-mtinfo.sh
+++ generate-mtinfo.sh
@@ -9,6 +9,7 @@ WORLDMODS_DIR=/tmp/technic_worldmods
 git clone --depth=1 https://gitlab.com/VanessaE/basic_materials.git ${WORLDMODS_DIR}/basic_materials
 git clone --depth=1 https://gitlab.com/VanessaE/pipeworks.git ${WORLDMODS_DIR}/pipeworks
 git clone --depth=1 https://gitlab.com/VanessaE/unifieddyes.git ${WORLDMODS_DIR}/unifieddyes
+git clone --depth=1 https://github.com/minetest-mods/moreblocks.git ${WORLDMODS_DIR}/moreblocks
 git clone --depth=1 https://github.com/BuckarooBanzay/mtinfo.git ${WORLDMODS_DIR}/mtinfo
 cp . ${WORLDMODS_DIR}/technic -R
 
