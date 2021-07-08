21fe6a5038f3a366ee8d0a09f44dff2851a7d2ca
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-07-08 07:34:52 +0200

    return default value on `technic.get_max_lag()` when the parsing fails

--- technic/max_lag.lua
+++ technic/max_lag.lua
@@ -8,8 +8,11 @@ local function explode(sep, input)
         return t
 end
 
+--- get the engines current max-lag value
+-- returns the current max-lag value or 0 if the value
+-- could not be parsed from the `/status` string
 function technic.get_max_lag()
 	local arrayoutput = explode(", ",minetest.get_server_status())
 	local arrayoutput2 = explode("=",arrayoutput[4])
-	return tonumber(arrayoutput2[1])
+	return tonumber(arrayoutput2[1]) or 0
 end
