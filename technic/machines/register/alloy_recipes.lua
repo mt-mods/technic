235656e78958a253654bbfd08b5799fc0971d9f8
nixnoxus <nixnoxus@web.de>
2021-08-02 21:17:58 +0200

    longer alloying time for digi cables (3x)

--- technic/machines/register/alloy_recipes.lua
+++ technic/machines/register/alloy_recipes.lua
@@ -40,12 +40,12 @@ if minetest.get_modpath("ethereal") then
 end
 
 if minetest.get_modpath("digilines") then
-	table.insert(recipes, {"technic:lv_cable",         "digilines:wire_std_00000000 2", "technic:lv_digi_cable"})
-	table.insert(recipes, {"technic:lv_cable_plate_1", "digilines:wire_std_00000000 2", "technic:lv_digi_cable_plate_1"})
-	table.insert(recipes, {"technic:mv_cable",         "digilines:wire_std_00000000 2", "technic:mv_digi_cable"})
-	table.insert(recipes, {"technic:mv_cable_plate_1", "digilines:wire_std_00000000 2", "technic:mv_digi_cable_plate_1"})
-	table.insert(recipes, {"technic:hv_cable",         "digilines:wire_std_00000000 2", "technic:hv_digi_cable"})
-	table.insert(recipes, {"technic:hv_cable_plate_1", "digilines:wire_std_00000000 2", "technic:hv_digi_cable_plate_1"})
+	table.insert(recipes, {"technic:lv_cable",         "digilines:wire_std_00000000 2", "technic:lv_digi_cable", 18})
+	table.insert(recipes, {"technic:lv_cable_plate_1", "digilines:wire_std_00000000 2", "technic:lv_digi_cable_plate_1", 18})
+	table.insert(recipes, {"technic:mv_cable",         "digilines:wire_std_00000000 2", "technic:mv_digi_cable", 18})
+	table.insert(recipes, {"technic:mv_cable_plate_1", "digilines:wire_std_00000000 2", "technic:mv_digi_cable_plate_1", 18})
+	table.insert(recipes, {"technic:hv_cable",         "digilines:wire_std_00000000 2", "technic:hv_digi_cable", 18})
+	table.insert(recipes, {"technic:hv_cable_plate_1", "digilines:wire_std_00000000 2", "technic:hv_digi_cable_plate_1", 18})
 end
 
 for _, data in pairs(recipes) do
