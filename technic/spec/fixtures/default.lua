8e87780b3fa199979c0101f57075f700ad49d006
SX <50966843+S-S-X@users.noreply.github.com>
2021-09-09 00:08:48 +0300

    Test power outages, grinding and compressing

--- technic/spec/fixtures/default.lua
+++ technic/spec/fixtures/default.lua
@@ -1,1 +1,11 @@
 minetest.register_node(":default:furnace", {})
+
+minetest.register_node( ":default:sand", {
+	description = "Sand",
+	drop = "default:sand",
+})
+
+minetest.register_node( ":default:sandstone", {
+	description = "Sandstone",
+	drop = "default:sandstone",
+})
