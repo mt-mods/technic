ddbd5960788b75f71926fc5110acd8f3503b3393
SX <50966843+S-S-X@users.noreply.github.com>
2021-10-19 07:05:33 +0300

    Revert "Add more config options"
    This reverts commit bff0b08831d42ab6e3dc9f1e6fd0a983c78cc656.

--- technic/config.lua
+++ technic/config.lua
@@ -1,39 +1,24 @@
 local config_file = minetest.get_worldpath() .. "/technic.conf"
 
 local defaults = {
-	-- Power tools enabled
-	enable_cans = "true",
-	enable_chainsaw = "true",
-	enable_flashlight = "false",
 	enable_mining_drill = "true",
 	enable_mining_laser = "false",
-	enable_multimeter = "true",
-	enable_prospector = "true",
-	enable_sonic_screwdriver = "true",
-	enable_tree_tap = "true",
-	enable_vacuum = "true",
-
-	-- Power tool options
-	multimeter_remote_start_ttl = "300",
-
-	-- Machine options
+	enable_flashlight = "false",
 	enable_wind_mill = "false",
 	enable_frames = "false",
-	enable_nuclear_reactor_digiline_selfdestruct = "false",
-	quarry_dig_above_nodes = "3",
-	quarry_max_depth = "100",
-	quarry_time_limit = "5000",
-
-	-- Power network and general options
-	switch_max_range = "256",
-	switch_off_delay_seconds = "1800",
-	network_overload_reset_time = "20",
-	admin_priv = "basic_privs",
 	enable_corium_griefing = "true",
 	enable_radiation_protection = "true",
 	enable_radiation_throttling = "false",
 	enable_entity_radiation_damage = "true",
 	enable_longterm_radiation_damage = "true",
+	enable_nuclear_reactor_digiline_selfdestruct = "false",
+	switch_max_range = "256",
+	switch_off_delay_seconds = "1800",
+	network_overload_reset_time = "20",
+	admin_priv = "basic_privs",
+	quarry_dig_above_nodes = "3",
+	quarry_max_depth = "100",
+	quarry_time_limit = "5000",
 	max_lag_reduction_multiplier = "0.99",
 	--constant_digit_count = nil,
 }
