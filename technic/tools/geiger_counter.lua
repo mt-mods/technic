32ce17e739322842b75076fc664a849e4da6bf50
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-11-21 17:47:17 +0100

    appy changes from @S-X-X

--- technic/tools/geiger_counter.lua
+++ technic/tools/geiger_counter.lua
@@ -4,12 +4,10 @@ local S = technic.getter
 local geiger_counter_max_charge = 30000
 technic.register_power_tool("technic:geiger_counter", geiger_counter_max_charge)
 
-minetest.register_tool("technic:geiger_counter", {
+technic.register_power_tool("technic:geiger_counter", {
 	description = S("Geiger counter"),
 	inventory_image = "technic_geiger_counter.png",
-	stack_max = 1,
-	wear_represents = "technic_RE_charge",
-	on_refill = technic.refill_RE_charge,
+	max_charge = geiger_counter_max_charge
 })
 
 minetest.register_craft({
