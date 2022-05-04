b26132a98b1edf1ac5957d4649b9a6b49787d106
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2022-05-04 15:00:30 +0200

    remove monitoring metrics (#266)
    * remove monitoring metrics
    
    * remove unused counter var
    
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>

--- technic/machines/switching_station_globalstep.lua
+++ technic/machines/switching_station_globalstep.lua
@@ -1,20 +1,3 @@
-
-local has_monitoring_mod = minetest.get_modpath("monitoring")
-
-local active_switching_stations_metric, switching_stations_usage_metric
-
-if has_monitoring_mod then
-	active_switching_stations_metric = monitoring.gauge(
-		"technic_active_switching_stations",
-		"Number of active switching stations"
-	)
-
-	switching_stations_usage_metric = monitoring.counter(
-		"technic_switching_stations_usage",
-		"usage in microseconds cpu time"
-	)
-end
-
 -- the interval between technic_run calls
 local technic_run_interval = 1.0
 local set_default_timeout = technic.set_default_timeout
@@ -46,13 +29,9 @@ minetest.register_globalstep(function(dtime)
 
 	local now = minetest.get_us_time()
 
-	local active_switches = 0
-
 	for network_id, network in pairs(technic.active_networks) do
 		if network.timeout > now and not technic.is_overloaded(network_id) then
 			-- station active
-			active_switches = active_switches + 1
-
 			if network.skip > 0 then
 				network.skip = network.skip - 1
 			else
@@ -91,13 +70,6 @@ minetest.register_globalstep(function(dtime)
 			technic.active_networks[network_id] = nil
 		end
 	end
-
-	if has_monitoring_mod then
-		local time_usage = minetest.get_us_time() - now
-		active_switching_stations_metric.set(active_switches)
-		switching_stations_usage_metric.inc(time_usage)
-	end
-
 end)
 
 minetest.register_chatcommand("technic_flush_switch_cache", {
