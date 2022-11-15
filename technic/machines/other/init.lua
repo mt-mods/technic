454958ecbf6e8c860813a740901eb2f44d3aef6e
OgelGames <olliverdc28@gmail.com>
2022-11-15 16:02:30 +1100

    remove frames, power radiator and lighting

--- technic/machines/other/init.lua
+++ technic/machines/other/init.lua
@@ -1,15 +1,12 @@
 local path = technic.modpath.."/machines/other"
 
--- mesecons and tubes related
+-- Mesecons and tubes related
 dofile(path.."/injector.lua")
 dofile(path.."/constructor.lua")
-
-if technic.config:get_bool("enable_frames") and minetest.get_modpath("mesecons_mvps") ~= nil then
-	dofile(path.."/frames.lua")
-end
 
 -- Coal-powered machines
 dofile(path.."/coal_alloy_furnace.lua")
 dofile(path.."/coal_furnace.lua")
 
+-- Force-loading
 dofile(path.."/anchor.lua")
