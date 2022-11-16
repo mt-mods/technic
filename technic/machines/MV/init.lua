72753c9e67497e32306dab4763f794395653d798
OgelGames <olliverdc28@gmail.com>
2022-11-16 15:49:53 +1100

    Remove frames, power radiator and lighting (#290)

--- technic/machines/MV/init.lua
+++ technic/machines/MV/init.lua
@@ -26,10 +26,3 @@ dofile(path.."/centrifuge.lua")
 dofile(path.."/tool_workshop.lua")
 
 dofile(path.."/freezer.lua")
-
--- The power radiator supplies appliances with inductive coupled power:
--- Lighting and associated textures is taken directly from VanessaE's homedecor and made electric.
--- This is currently useless, slow, and mostly copied
---dofile(path.."/power_radiator.lua")
---dofile(path.."/lighting.lua")
-
