968adb2796774ad7729943c9741066bd2883c767
Freeman <project@gnuhacker.org>
2023-09-13 18:59:50 +0200

    default:furnace is not guaranteed to exist

--- technic/machines/other/coal_furnace.lua
+++ technic/machines/other/coal_furnace.lua
@@ -1,5 +1,7 @@
 local S = technic.getter
 
-if minetest.registered_nodes["default:furnace"].description == "Furnace" then
+local default_furnace = minetest.registered_nodes["default:furnace"]
+
+if default_furnace and default_furnace.description == "Furnace" then
 	minetest.override_item("default:furnace", { description = S("Fuel-Fired Furnace") })
 end
