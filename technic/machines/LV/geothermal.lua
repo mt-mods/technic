1eba579a26f1550420611c5bc4d75c738276c423
Github is a non-free platform owned by Microsoft. Reasonable alternatives exist, such as Gitea, Sourcehut. We need a federated, mastodon-like forge based on ForgeFed. See: https://forgefed.org <project@gnuhacker.org>
2023-12-16 13:59:54 +0100

    Update technic/machines/LV/geothermal.lua
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

--- technic/machines/LV/geothermal.lua
+++ technic/machines/LV/geothermal.lua
@@ -110,7 +110,7 @@ minetest.register_node("technic:geothermal_active", {
 	         "technic_geothermal_side.png", "technic_geothermal_side.png", "technic_geothermal_side.png"},
 	paramtype2 = "facedir",
 	groups = {snappy=2, choppy=2, oddly_breakable_by_hand=2,
-		  technic_machine=1, technic_lv=1, not_in_creative_inventory=1, axey=2, handy=1},
+		technic_machine=1, technic_lv=1, not_in_creative_inventory=1, axey=2, handy=1},
 	_mcl_blast_resistance = 1,
 	_mcl_hardness = 0.8,
 	legacy_facedir_simple = true,
