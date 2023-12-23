c3a7434ebdf0896688e083e28e8210934ffd90dd
Freeman <project@gnuhacker.org>
2023-12-23 22:30:24 +0100

    sonic screwdriver conf revert

--- technic/tools/init.lua
+++ technic/tools/init.lua
@@ -1,6 +1,5 @@
 local path = technic.modpath.."/tools"
 local mesecons_materials = minetest.get_modpath("mesecons_materials")
-local has_mcl = minetest.get_modpath("mcl_core")
 
 local function enabled(name)
 	return technic.config:get_bool("enable_" .. name)
@@ -12,7 +11,7 @@ if enabled("flashlight")        then dofile(path.."/flashlight.lua") end
 if enabled("cans")              then dofile(path.."/cans.lua") end
 if enabled("chainsaw")          then dofile(path.."/chainsaw.lua") end
 if enabled("tree_tap")          then dofile(path.."/tree_tap.lua") end
-if enabled("sonic_screwdriver") and mesecons_materials and not has_mcl then dofile(path.."/sonic_screwdriver.lua") end
+if enabled("sonic_screwdriver") and mesecons_materials then dofile(path.."/sonic_screwdriver.lua") end
 if enabled("prospector")        then dofile(path.."/prospector.lua") end
 if enabled("vacuum")            then dofile(path.."/vacuum.lua") end
 if enabled("multimeter")        then dofile(path.."/multimeter.lua") end
