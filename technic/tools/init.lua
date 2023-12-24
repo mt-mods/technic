dea091889aead0c5c486a291944d7251215bde0b
Freeman <project@gnuhacker.org>
2023-12-24 12:14:40 +0100

    screwdriver depend not mesecons mat

--- technic/tools/init.lua
+++ technic/tools/init.lua
@@ -1,5 +1,4 @@
 local path = technic.modpath.."/tools"
-local mesecons_materials = minetest.get_modpath("mesecons_materials")
 
 local function enabled(name)
 	return technic.config:get_bool("enable_" .. name)
@@ -11,7 +10,7 @@ if enabled("flashlight")        then dofile(path.."/flashlight.lua") end
 if enabled("cans")              then dofile(path.."/cans.lua") end
 if enabled("chainsaw")          then dofile(path.."/chainsaw.lua") end
 if enabled("tree_tap")          then dofile(path.."/tree_tap.lua") end
-if enabled("sonic_screwdriver") and mesecons_materials then dofile(path.."/sonic_screwdriver.lua") end
+if enabled("sonic_screwdriver") then dofile(path.."/sonic_screwdriver.lua") end
 if enabled("prospector")        then dofile(path.."/prospector.lua") end
 if enabled("vacuum")            then dofile(path.."/vacuum.lua") end
 if enabled("multimeter")        then dofile(path.."/multimeter.lua") end
