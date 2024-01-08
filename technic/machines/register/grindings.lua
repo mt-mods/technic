87c26124545485aa120ef3179d1935c67ff02ffc
OgelGames <olliverdc28@gmail.com>
2024-01-08 18:21:08 +1100

    make use of group recipes for tree grindings

--- technic/machines/register/grindings.lua
+++ technic/machines/register/grindings.lua
@@ -45,29 +45,15 @@ local function register_tree_grinding(name, tree, wood, extract, grinding_color)
 	end
 end
 
-local rubber_tree_planks = moretrees and "moretrees:rubber_tree_planks"
+local rubber_planks = moretrees and "moretrees:rubber_tree_planks"
 local default_extract = dye and "dye:brown 2"
-
 -- https://en.wikipedia.org/wiki/Catechu ancient brown dye from the wood of acacia trees
 local acacia_extract = dye and "dye:brown 8"
 
--- technic recipes don't support groups yet :/
---register_tree_grinding("Common Tree", "group:tree", "group:wood", default_extract)
-
+-- Specific recipes for acacia and rubber trees
 register_tree_grinding("Acacia", "default:acacia_tree", "default:acacia_wood", acacia_extract)
-register_tree_grinding("Common Tree", "default:tree", "default:wood", default_extract)
-register_tree_grinding("Common Tree", "default:aspen_tree", "default:aspen_wood", default_extract)
-register_tree_grinding("Common Tree", "default:jungletree", "default:junglewood", default_extract)
-register_tree_grinding("Common Tree", "default:pine_tree", "default:pine_wood", default_extract)
-register_tree_grinding("Rubber Tree", "moretrees:rubber_tree_trunk", rubber_tree_planks, "technic:raw_latex")
+register_tree_grinding("Rubber Tree", "moretrees:rubber_tree_trunk", rubber_planks, "technic:raw_latex 2")
 register_tree_grinding("Rubber Tree", "moretrees:rubber_tree_trunk_empty", nil, "technic:raw_latex")
 
-if moretrees then
-	local trees = {
-		"beech", "apple_tree", "oak", "sequoia", "birch", "palm",
-		"date_palm", "spruce", "cedar", "poplar", "willow", "fir"
-	}
-	for _,tree in pairs(trees) do
-		register_tree_grinding("Common Tree", "moretrees:"..tree.."_trunk", "moretrees:"..tree.."_planks", default_extract)
-	end
-end
+-- Group recipe for all other trees
+register_tree_grinding("Common Tree", "group:tree", "group:wood", default_extract)
