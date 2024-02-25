b734cd8b7b2b9373b2000ba8ab5d2771dfbdc6ce
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-02-26 00:13:32 +0100

    Update injector.lua

--- technic/machines/other/injector.lua
+++ technic/machines/other/injector.lua
@@ -71,6 +71,7 @@ minetest.register_node("technic:injector", {
 	},
 	paramtype2 = "facedir",
 	groups = {snappy=2, choppy=2, oddly_breakable_by_hand=2, tubedevice=1, tubedevice_receiver=1, axey=2, handy=1},
+	is_ground_content = false,
 	_mcl_blast_resistance = 1,
 	_mcl_hardness = 0.8,
 	tube = {
