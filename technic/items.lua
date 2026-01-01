0a65ccc4e0a2e8a16f8675641286d19e229ffb2a
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-01-01 06:54:30 -0500

    use nondeprecated namespace

--- technic/items.lua
+++ technic/items.lua
@@ -1,31 +1,31 @@
 
 local S = technic.getter
 
-local has_mcl = minetest.get_modpath("mcl_core")
+local has_mcl = core.get_modpath("mcl_core")
 
-minetest.register_craftitem("technic:silicon_wafer", {
+core.register_craftitem("technic:silicon_wafer", {
 	description = S("Silicon Wafer"),
 	inventory_image = "technic_silicon_wafer.png",
 })
 
-minetest.register_craftitem( "technic:doped_silicon_wafer", {
+core.register_craftitem( "technic:doped_silicon_wafer", {
 	description = S("Doped Silicon Wafer"),
 	inventory_image = "technic_doped_silicon_wafer.png",
 })
 
-minetest.register_craftitem("technic:uranium_fuel", {
+core.register_craftitem("technic:uranium_fuel", {
 	description = S("Uranium Fuel"),
 	inventory_image = "technic_uranium_fuel.png",
 })
 
-minetest.register_craftitem( "technic:diamond_drill_head", {
+core.register_craftitem( "technic:diamond_drill_head", {
 	description = S("Diamond Drill Head"),
 	inventory_image = "technic_diamond_drill_head.png",
 })
 
 technic.register_power_tool("technic:blue_energy_crystal", {
 	description = S("Blue Energy Crystal"),
-	inventory_image = minetest.inventorycube(
+	inventory_image = core.inventorycube(
 		"technic_diamond_block_blue.png",
 		"technic_diamond_block_blue.png",
 		"technic_diamond_block_blue.png"),
@@ -35,7 +35,7 @@ technic.register_power_tool("technic:blue_energy_crystal", {
 
 technic.register_power_tool("technic:green_energy_crystal", {
 	description = S("Green Energy Crystal"),
-	inventory_image = minetest.inventorycube(
+	inventory_image = core.inventorycube(
 		"technic_diamond_block_green.png",
 		"technic_diamond_block_green.png",
 		"technic_diamond_block_green.png"),
@@ -45,7 +45,7 @@ technic.register_power_tool("technic:green_energy_crystal", {
 
 technic.register_power_tool("technic:red_energy_crystal", {
 	description = S("Red Energy Crystal"),
-	inventory_image = minetest.inventorycube(
+	inventory_image = core.inventorycube(
 		"technic_diamond_block_red.png",
 		"technic_diamond_block_red.png",
 		"technic_diamond_block_red.png"),
@@ -53,62 +53,62 @@ technic.register_power_tool("technic:red_energy_crystal", {
 	max_charge = 50000,
 })
 
-minetest.register_craftitem("technic:copper_coil", {
+core.register_craftitem("technic:copper_coil", {
 	description = S("Copper Coil"),
 	inventory_image = "technic_copper_coil.png",
 })
 
-minetest.register_craftitem("technic:lv_transformer", {
+core.register_craftitem("technic:lv_transformer", {
 	description = S("Low Voltage Transformer"),
 	inventory_image = "technic_lv_transformer.png",
 })
 
-minetest.register_craftitem("technic:mv_transformer", {
+core.register_craftitem("technic:mv_transformer", {
 	description = S("Medium Voltage Transformer"),
 	inventory_image = "technic_mv_transformer.png",
 })
 
-minetest.register_craftitem( "technic:hv_transformer", {
+core.register_craftitem( "technic:hv_transformer", {
 	description = S("High Voltage Transformer"),
 	inventory_image = "technic_hv_transformer.png",
 })
 
-minetest.register_craftitem( "technic:control_logic_unit", {
+core.register_craftitem( "technic:control_logic_unit", {
 	description = S("Control Logic Unit"),
 	inventory_image = "technic_control_logic_unit.png",
 })
 
-minetest.register_craftitem("technic:mixed_metal_ingot", {
+core.register_craftitem("technic:mixed_metal_ingot", {
 	description = S("Mixed Metal Ingot"),
 	inventory_image = "technic_mixed_metal_ingot.png",
 })
 
-minetest.register_craftitem("technic:composite_plate", {
+core.register_craftitem("technic:composite_plate", {
 	description = S("Composite Plate"),
 	inventory_image = "technic_composite_plate.png",
 })
 
-minetest.register_craftitem("technic:copper_plate", {
+core.register_craftitem("technic:copper_plate", {
 	description = S("Copper Plate"),
 	inventory_image = "technic_copper_plate.png",
 })
 
-minetest.register_craftitem("technic:carbon_plate", {
+core.register_craftitem("technic:carbon_plate", {
 	description = S("Carbon Plate"),
 	inventory_image = "technic_carbon_plate.png",
 })
 
-minetest.register_craftitem("technic:graphite", {
+core.register_craftitem("technic:graphite", {
 	description = S("Graphite"),
 	inventory_image = "technic_graphite.png",
 })
 
-minetest.register_craftitem("technic:carbon_cloth", {
+core.register_craftitem("technic:carbon_cloth", {
 	description = S("Carbon Cloth"),
 	inventory_image = "technic_carbon_cloth.png",
 })
 
-minetest.register_node("technic:machine_casing", {
+core.register_node("technic:machine_casing", {
 	description = S("Machine Casing"),
 	groups = {cracky=2, pickaxey=2},
 	is_ground_content = false,
@@ -126,8 +126,8 @@ for p = 0, 35 do
 	local psuffix = p == 7 and "" or p
 	local ingot = "technic:uranium"..psuffix.."_ingot"
 	local block = "technic:uranium"..psuffix.."_block"
-	local ov = p == 7 and minetest.override_item or nil;
-	(ov or minetest.register_craftitem)(ingot, {
+	local ov = p == 7 and core.override_item or nil;
+	(ov or core.register_craftitem)(ingot, {
 		description = S("@1%-Fissile Uranium Ingot", string.format("%.1f", p/10)),
 		inventory_image = "technic_uranium_ingot.png",
 		groups = {uranium_ingot=1, not_in_creative_inventory=nici},
@@ -164,7 +164,7 @@ for p = 0, 35 do
 	-- for a fully-depleted uranium block and radioactive=2.6 for
 	-- a 3.5%-fissile uranium block.
 	local radioactivity = math.floor(math.sqrt((1+5.55*p/35) * 18 / (1+5.55*7/35)) + 0.5);
-	(ov or minetest.register_node)(block, {
+	(ov or core.register_node)(block, {
 		description = S("@1%-Fissile Uranium Block", string.format("%.1f", p/10)),
 		tiles = {"technic_uranium_block.png"},
 		is_ground_content = false,
@@ -175,7 +175,7 @@ for p = 0, 35 do
 		sounds = technic.sounds.node_sound_stone_defaults(),
 	});
 	if not ov then
-		minetest.register_craft({
+		core.register_craft({
 			output = block,
 			recipe = {
 				{ingot, ingot, ingot},
@@ -183,7 +183,7 @@ for p = 0, 35 do
 				{ingot, ingot, ingot},
 			},
 		})
-		minetest.register_craft({
+		core.register_craft({
 			output = ingot.." 9",
 			recipe = {{block}},
 		})
